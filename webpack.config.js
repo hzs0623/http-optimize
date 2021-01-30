41b251004d9c1ad685cce4df6ed51e8ff68d8c56
yifangsx <yifangsx@fordeal.com>
2021-01-30 23:08:18 +0800

    初始化项目

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+// 对js代码进行混淆压缩的插件
+const uglifyJSPlugin = new UglifyJSPlugin();
+
+// 对babel的配置，内容同.babelrc文件
+const babelOptions = {
+  "presets": [
+    ["env", {
+      "targets": {
+        "browsers": ["last 2 versions", "safari >= 7"]
+      }
+    }]
+  ]
+}
+module.exports = {
+  module: {
+    rules: [{
+      test: /\.ts(x?)$/,
+      exclude: /node_modules/,
+      use: [
+        {
+          loader: 'babel-loader',
+          options: babelOptions
+        },
+        {
+          loader: 'ts-loader'
+        }
+      ]
+    }]},
+  devtool: 'cheap-module-source-map',
+  plugins: [
+    uglifyJSPlugin,
+  ],
+  entry: ['babel-polyfill', './src/main.ts'],
+  resolve:
+    {
+      extensions: ['.ts', '.js', '.json']
+    },
+  // 设置出口文件地址与文件名
+  output: {
+    path: path.resolve('dist'),
+    filename: 'index.js'
+  },
+};
